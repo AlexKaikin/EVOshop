1e85b75fd6787cb87c45b0f8254ad3a06e0dcd0c
Alex Kaikin <lex.kaikin@gmail.com>
2022-07-21 20:49:29 +0400

    незначительные правки вёрстки

--- config/settings.py
+++ config/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'apps/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'apps/static/')
 ]
 
 MEDIA_URL = 'media/'
