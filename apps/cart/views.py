01b3f6204c30cd7957e386175a602e8d7780395a
Alex Kaikin <lex.kaikin@gmail.com>
2022-07-27 11:50:00 +0400

    реализована возможность изменения количества товара в корзине

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -29,6 +29,8 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'cart/cart.html', {'cart': cart})
 
 
